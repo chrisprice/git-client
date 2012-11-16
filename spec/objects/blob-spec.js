e504245790d8cbb3393944f3b999ebf710300b45
Chris Price <price.c@gmail.com>
2012-11-16 17:29:51 +0000

    more quick commit of everythings

---
+++ spec/objects/blob-spec.js
@@ -0,0 +1,21 @@
+var fs = require('../../lib/objects/fs'),
+    blob = require('../../lib/objects/blob'),
+    data = require('./data');
+
+describe('Parsing/composing a blob', function() {
+  data();
+
+  it('Should compose a blob', function(done) {
+    done();
+  });
+
+  it('Should parse a blob', function(done) {
+    done();
+  });
+
+});
+
+
+
+
+
--- spec/objects/fs-spec.js
+++ spec/objects/fs-spec.js
@@ -1,10 +1,10 @@
-var fs = require('../../lib/objects/fs')
+var fs = require('../../lib/objects/fs'),
     data = require('./data');
 
 describe('Reading/writing an object to a file', function() {
   data();
 
-  it('Should write a buffer successfully', function(done) {
+  it('Should write a buffer', function(done) {
     fs.write(new Buffer("blob 16\000what is up, doc?"), function(error, hash) {
       expect(error).toBeNull();
       expect(hash).toBe("bd9dbf5aae1a3862dd1526723246b20206e5fc37");
@@ -12,7 +12,7 @@ describe('Reading/writing an object to a file', function() {
     });
   });
 
-  it('Should read to a buffer successfully', function(done) {
+  it('Should read to a buffer', function(done) {
     fs.read("bd9dbf5aae1a3862dd1526723246b20206e5fc37", function(error, buffer) {
       expect(error).toBeNull();
       expect(buffer.toString()).toBe("blob 16\000what is up, doc?");
